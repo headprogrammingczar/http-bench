8d966e9350c26e5c62b85406911b4c70ca958834
databox <headprogrammingczar@gmail.com>
2013-05-04 18:13:48 -0400

    runnable version, needs tuning

--- Main.hs
+++ Main.hs
@@ -5,32 +5,86 @@ import Control.Concurrent
 import Control.Monad
 
 import Network.HTTP
+import Text.Printf
 
 import System.Exit
 import System.Environment
+import Control.Exception as E
+
+data Stats = Stats {hits :: Int, prevHits :: Int} deriving (Show)
+
+interval = 10 * 1000 * 1000 -- ten seconds
 
 main = do
   args <- getArgs
   when (length args /= 3) (fail usage)
-  let [url, totalStr, concurrentStr] = args
-  total <- readIO totalStr
-  concurrent <- readIO concurrentStr
-  queue <- newChan
-  count <- newMVar 0
-  forM_ [1 .. concurrent] $ \_ -> forkIO $ go count queue
-  forM_ [1 .. total] $ \_ -> writeChan queue $ do
-    simpleHTTP (getRequest url)
-    return ()
-  forever $ do
-    n <- readMVar count
-    when (n == total) exitSuccess
-    threadDelay 100
+  let [url, minConcStr, maxConcStr] = args
+  minConc <- readIO minConcStr :: IO Int
+  maxConc <- readIO maxConcStr :: IO Int
+  currentConcurrent <- newMVar minConc
+  stats <- newMVar $ Stats {hits = 0, prevHits = 0}
+  flip E.catch (handleInterrupts stats) $ do
+    -- create initial set of threads
+    threads <- forM [1 .. minConc] $ \_ -> forkIO $ go url stats
+    -- spawn thread for pool control
+    forkIO $ poolControl threads (go url stats) currentConcurrent
+    -- main thread does stat control
+    statControl minConc maxConc currentConcurrent stats
 
-go :: MVar Int -> Chan (IO ()) -> IO ()
-go count queue = forever $ do
-  join (readChan queue)
-  modifyMVar_ count (return . succ)
+handleInterrupts stats e | e /= UserInterrupt = E.throwIO e
+                         | otherwise = do s <- readMVar stats
+                                          putStr "\n\n"
+                                          print s
+                                          error "Exiting..."
 
-usage = "Usage: http-bench <url> <total> <concurrent>\n\
-        \    Benchmark a website by requesting a URL many times concurrently\n"
+poolControl :: [ThreadId] -> IO () -> MVar Int -> IO ()
+poolControl threads action currentConcurrent = do
+  -- maintain a list of type [ThreadId] that represents a threadpool
+  threadDelay interval
+  let currentThreads = length threads
+  wantedThreads <- readMVar currentConcurrent
+  -- periodically spawn or kill threads as needed to keep the pool at the size specified by an mvar
+  case compare wantedThreads currentThreads of
+    GT -> do let newThreads = wantedThreads - currentThreads
+             tids <- forM [1 .. newThreads] $ \_ -> forkIO action
+             poolControl (tids ++ threads) action currentConcurrent
+    LT -> do let removeThreads = currentThreads - wantedThreads
+                 (remove, keep) = splitAt removeThreads threads
+             forM_ remove $ \tid -> killThread tid
+             poolControl keep action currentConcurrent
+    EQ -> poolControl threads action currentConcurrent
+
+statControl :: Int -> Int -> MVar Int -> MVar Stats -> IO ()
+statControl minConc maxConc currentConcurrent statsRef = forever $ do
+  threadDelay interval
+  -- read current stats information
+  stats <- readMVar statsRef
+  conc <- readMVar currentConcurrent
+  -- use information from stats to update concurrency level, if necessary
+  -- if we end up unable to rise above the minimum concurrency level, print message
+  let wanted = case (prevHits stats `compare` hits stats) of
+                      EQ -> conc
+                      LT -> min maxConc (conc + 1)
+                      GT -> max minConc (conc - 1)
+  -- if we end up stable at the maximum concurrency level, print message
+  printf "Hits: %i - Concurrent: %i\n" (hits stats) wanted
+  -- reset stats for current interval
+  modifyMVar_ statsRef (return . reset)
+  modifyMVar_ currentConcurrent (return . const wanted)
+  return ()
+
+reset :: Stats -> Stats
+reset s = s {hits = 0, prevHits = hits s}
+
+go :: String -> MVar Stats -> IO ()
+go url stats = forever $ do
+  simpleHTTP (getRequest url)
+  modifyMVar_ stats $ \s -> return s {hits = hits s + 1}
+
+usage = "\n\
+        \Usage: http-bench <url> <min-concurrent> <max-concurrent>\n\
+        \    Benchmark a website by requesting a URL many times concurrently.\n\
+        \    http-bench will begin at the minimum number of concurrent requests,\n\
+        \    and slowly scale to the speed of your webserver, or the upper\n\
+        \    concurrency limit parameter.\n"
 
